34d6d572f595f53e5183969cb48f2a47f60a737f
Shayan <skhos068@uottawa.ca>
2023-07-12 13:48:37 -0400

    created initial container for header and body

---
+++ src/components/header/navbar/index.tsx
@@ -0,0 +1,17 @@
+import { Nav, NavbarContainer} from './navbarElements';
+import React, { useState } from 'react';
+
+const Navbar = () => {
+
+   return (
+       <>
+          <Nav>
+               <NavbarContainer>
+                 
+               </NavbarContainer>
+           </Nav>
+       </>
+   );
+};
+
+export default Navbar;
