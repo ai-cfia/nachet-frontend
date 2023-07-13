8672b9f12b1244f60a758e6a45296f9999f3d0f0
Shayan <skhos068@uottawa.ca>
2023-07-13 11:55:28 -0400

    updated

--- src/components/body/videofeed/index.tsx
+++ src/components/body/videofeed/index.tsx
@@ -1,10 +1,12 @@
 import { VideoFeedContainer } from './indexElements';
+import React from 'react';
+import Webcam from "react-webcam";
 
 const VideoFeed = () => {
 
    return (
           <VideoFeedContainer>
-
+            <Webcam audio={false} mirrored={false} />
            </VideoFeedContainer>
    );
 };
