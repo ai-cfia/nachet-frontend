f43b25e924e9ba5ea2ca69b7cf1a75764a43bff5
CFIALeronB <Leron.Bergelson@inspection.gc.ca>
2024-03-18 16:40:51 -0400

    Fix ESLint error by adding eslint-plugin-react
    - Installed `eslint-plugin-react` to resolve ESLint configuration issues.
    - This update ensures ESLint can properly lint React codebase as per the project's `.eslintrc.cjs` settings.
    
    This change addresses the ESLint error encountered when running `npm run lint`, ensuring a smoother development experience.
    
    #104

--- src/_versions.ts
+++ src/_versions.ts
@@ -11,8 +11,8 @@ export interface TsAppVersion {
 export const versions: TsAppVersion = {
     version: '0.4.0',
     name: 'nachet-frontend',
-    versionDate: '2024-03-18T19:24:08.068Z',
-    gitCommitHash: '12fc77e',
-    versionLong: '0.4.0-12fc77e',
+    versionDate: '2024-03-18T20:40:09.774Z',
+    gitCommitHash: 'b4e641e',
+    versionLong: '0.4.0-b4e641e',
 };
 export default versions;
