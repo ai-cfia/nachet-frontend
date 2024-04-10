7480c68b74bf83b53a17841434566638536c992c
ChromaticPanic <78883122+ChromaticPanic@users.noreply.github.com>
2024-04-10 00:40:40 +0000

    common module export

--- src/common/index.ts
+++ src/common/index.ts
@@ -1,3 +1,36 @@
-import { DecodeError, FetchError, BlobError } from "./error";
+import { DecodeError, FetchError, BlobError, AzureAPIError } from "./error";
+import {
+  readAzureStorageDir,
+  createAzureStorageDir,
+  deleteAzureStorageDir,
+  inferenceRequest,
+  fetchModelMetadata,
+} from "./api";
+import {
+  loadToCanvas,
+  decodeTiff,
+  getImageDims,
+  nextCacheIndex,
+  loadCaptureToCache,
+  loadResultsToCache,
+  getLabelOccurrence,
+} from "./cacheutils";
 
-export { DecodeError, FetchError, BlobError };
+export {
+  AzureAPIError,
+  DecodeError,
+  FetchError,
+  BlobError,
+  readAzureStorageDir,
+  createAzureStorageDir,
+  deleteAzureStorageDir,
+  inferenceRequest,
+  fetchModelMetadata,
+  loadToCanvas,
+  decodeTiff,
+  getImageDims,
+  nextCacheIndex,
+  loadCaptureToCache,
+  loadResultsToCache,
+  getLabelOccurrence,
+};
