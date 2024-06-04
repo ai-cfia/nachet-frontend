b13112f50e2b07bc266bb24be94f2e461ea3c4f6
ChromaticPanic <78883122+ChromaticPanic@users.noreply.github.com>
2024-06-04 09:54:51 -0500

    set canvas image object-fit to fit

--- src/components/body/microscope_feed/indexElements.tsx
+++ src/components/body/microscope_feed/indexElements.tsx
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const Canvas = styled.canvas`
   height: 100%;
   width: 100%;
-  object-fit: fill;
+  object-fit: fit;
   object-cover: cover;
   cover: contain;
 `;
