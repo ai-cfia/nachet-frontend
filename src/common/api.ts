48cffd647c10419dda82baf67a9d60ef49dcbab4
Joseffus Santos <joseffus.santos@inspection.gc.ca>
2025-03-11 23:29:57 +0000

    Remove debug console log from API request function

--- src/common/api.ts
+++ src/common/api.ts
@@ -275,7 +275,6 @@ export const requestClassList = async (
   if (backendUrl === "" || backendUrl == null) {
     throw new ValueError("Backend URL is null or empty");
   }
-  console.log("backendUrl", backendUrl);
   // if (uuid === "" || uuid == null) {
   //   throw new ValueError("UUID is null or empty");
   // }
